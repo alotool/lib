a94503016f4bcc2b3b3bcb3dc4a53fc93d65cb70
ALOTOOL <alotool360@gmail.com>
2020-12-11 22:25:15 +0700

    Initial commit

--- js/plugins/contact-us-btn/README.md
+++ js/plugins/contact-us-btn/README.md
@@ -1,1 +1,17 @@
 # CONTACT-US-BTN
+
+### Publish package to NPM package registry
+```shell script
+npm login
+npm publish --access public
+```
+
+### Unpublishing a single version of a package
+```shell script
+npm unpublish <package-name>@<version>
+```
+
+### Unpublishing an entire package
+```shell script
+npm unpublish <package-name> -f
+```
---
+++ js/plugins/contact-us-btn/gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp');
+const del = require('del');
+const concat = require('gulp-concat');
+const size = require('gulp-size');
+const notify = require('gulp-notify');
+const uglify = require('gulp-uglify');
+const csso = require('gulp-csso');
+
+function clean() {
+  return del(['dist']);
+}
+
+function styles() {
+  return gulp.src('./src/contact-us-btn.css')
+    .pipe(concat('contact-us-btn.css'))
+    .pipe(gulp.dest('./dist'))
+    .pipe(concat('contact-us-btn.min.css'))
+    .pipe(csso({discardComments: false}))
+    .pipe(gulp.dest('./dist'))
+    .pipe(notify('Styles build done successfully!'))
+    .pipe(size({showFiles: true}));
+}
+
+function scripts() {
+  return gulp.src('./src/contact-us-btn.js')
+    .pipe(concat('contact-us-btn.js'))
+    .pipe(gulp.dest('./dist'))
+    .pipe(concat('contact-us-btn.min.js'))
+    .pipe(uglify({mangle: true}))
+    .pipe(gulp.dest('./dist'))
+    .pipe(notify('Scripts build done successfully!'))
+    .pipe(size({showFiles: true}));
+}
+
+const build = gulp.series(clean, gulp.parallel(styles, scripts));
+
+exports.clean = clean;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.default = build;
