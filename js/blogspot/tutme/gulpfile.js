1f03d206932ac1d349cb0c32a37df9ebc4afb820
alotool <alotool360@gmail.com>
2021-04-21 01:02:18 +0700

    Initial commit

--- js/blogspot/tutme/gulpfile.js
+++ js/blogspot/tutme/gulpfile.js
@@ -1,20 +1,20 @@
-const gulp = require('gulp');
-const del = require('del');
+const gulp   = require('gulp');
+const del    = require('del');
 const concat = require('gulp-concat');
-const size = require('gulp-size');
+const size   = require('gulp-size');
 const notify = require('gulp-notify');
 const uglify = require('gulp-uglify');
-const csso = require('gulp-csso');
+const csso   = require('gulp-csso');
 
 function clean() {
   return del(['dist']);
 }
 
 function styles() {
-  return gulp.src('./src/w3pro.css')
-    .pipe(concat('w3pro.css'))
+  return gulp.src('./src/tutme.css')
+    .pipe(concat('tutme.css'))
     .pipe(gulp.dest('./dist'))
-    .pipe(concat('w3pro.min.css'))
+    .pipe(concat('tutme.min.css'))
     .pipe(csso({discardComments: false}))
     .pipe(gulp.dest('./dist'))
     .pipe(notify('Styles build done successfully!'))
@@ -22,10 +22,10 @@ function styles() {
 }
 
 function scripts() {
-  return gulp.src('./src/w3pro.js')
-    .pipe(concat('w3pro.js'))
+  return gulp.src('./src/tutme.js')
+    .pipe(concat('tutme.js'))
     .pipe(gulp.dest('./dist'))
-    .pipe(concat('w3pro.min.js'))
+    .pipe(concat('tutme.min.js'))
     .pipe(uglify({mangle: true}))
     .pipe(gulp.dest('./dist'))
     .pipe(notify('Scripts build done successfully!'))
@@ -34,7 +34,7 @@ function scripts() {
 
 const build = gulp.series(clean, gulp.parallel(styles, scripts));
 
-exports.clean = clean;
-exports.styles = styles;
+exports.clean   = clean;
+exports.styles  = styles;
 exports.scripts = scripts;
 exports.default = build;
