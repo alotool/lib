f83a3f803e079c08393bdb346392f9eea9ebc422
alotool <alotool360@gmail.com>
2021-06-25 19:11:30 +0700

    Initial commit

--- js/plugins/busy-load/src/lib/defaults.js
+++ js/plugins/busy-load/src/lib/defaults.js
@@ -1,5 +1,5 @@
 export default  {
-    spinner: "pump", // pump, accordion, pulsar, cube, cubes, circle-line, circles, cube-grid
+    spinner: "pump", // pump, accordion, pulsar, cube, cubes, circle-line, circles, cube-grid, rect
     image: false,
     fontawesome: false, // "fa fa-refresh fa-spin fa-2x fa-fw"
     custom: false, // jQuery Object
@@ -9,14 +9,14 @@ export default  {
     minSize: "20px", // Integer/String only for spinners & images, not fontawesome & custom
     text: false,
     textColor: false, // default is color
-    textMargin: ".5rem", 
-    textPosition: "right", // left, right, top, bottom  
+    textMargin: ".5rem",
+    textPosition: "right", // left, right, top, bottom
     fontSize: "1rem",
-    fullScreen: false, 
+    fullScreen: false,
     animation: false,	// fade, slide
-    animationDuration: "fast",	// String, Integer 
-    containerClass: "busy-load-container", 
-    containerItemClass: "busy-load-container-item", 
-    spinnerClass: "busy-load-spinner", 
-    textClass: "busy-load-text",  
+    animationDuration: "fast",	// String, Integer
+    containerClass: "busy-load-container",
+    containerItemClass: "busy-load-container-item",
+    spinnerClass: "busy-load-spinner",
+    textClass: "busy-load-text",
 };
